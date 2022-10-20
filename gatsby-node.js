c806bd65afb6a54d9e34283af2116236957a205e
mdotyabut <mark.yabut@ryerson.ca>
2022-10-19 21:52:34 -0400

    missing bracket

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,27 +3,28 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     actions.setWebpackConfig({
       module: {
         rules: [
-        {
-          test: /@react-three\/fiber/,
-          use: loaders.null(),
-        },
-        {
-          test: /@react-three\/postprocessing/,
-          use: loaders.null(),
-        },
-        {
-          test: /@react-three\/drei/,
-          use: loaders.null(),
-        },
-        {
-          test: /three/,
-          use: loaders.null(),
-        },
-        {
-          test: /postprocessing/,
-          use: loaders.null(),
-        },
-      ],
-    },
-  })
+          {
+            test: /@react-three\/fiber/,
+            use: loaders.null(),
+          },
+          {
+            test: /@react-three\/postprocessing/,
+            use: loaders.null(),
+          },
+          {
+            test: /@react-three\/drei/,
+            use: loaders.null(),
+          },
+          {
+            test: /three/,
+            use: loaders.null(),
+          },
+          {
+            test: /postprocessing/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
 }
