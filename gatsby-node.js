4ce2c9c0950450b86f3bd5648030807fc2cf9638
mdotyabut <mark.yabut@ryerson.ca>
2022-10-19 21:34:49 -0400

    trying to fix build issue pt1

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,29 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+        {
+          test: /@react-three\/fiber/,
+          use: loaders.null(),
+        },
+        {
+          test: /@react-three\/postprocessing/,
+          use: loaders.null(),
+        },
+        {
+          test: /@react-three\/drei/,
+          use: loaders.null(),
+        },
+        {
+          test: /three/,
+          use: loaders.null(),
+        },
+        {
+          test: /postprocessing/,
+          use: loaders.null(),
+        },
+      ],
+    },
   })
 }
